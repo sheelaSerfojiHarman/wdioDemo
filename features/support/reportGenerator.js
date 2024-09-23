12aa764102208f974007867eb1851fe97a41988a
sheelaSerfojiHarman <118346696+sheelaSerfojiHarman@users.noreply.github.com>
2024-09-23 17:20:52 +0530

    Update reportGenerator.js

--- features/support/reportGenerator.js
+++ features/support/reportGenerator.js
@@ -26,21 +26,3 @@ report.generate({
     ]
   }
 });
-
-var options = {
-  theme: 'bootstrap',
-  jsonFile: './reports/cucumber_report.json',
-  output: './reports/cucumber_report.html',
-  reportSuiteAsScenarios: true,
-  launchReport: true,
-  metadata: {
-    "App Version": "1.0.0",
-    "Test Environment": "STAGING",
-    "Browser": "Chrome  90.0.4430.93",
-    "Platform": "Windows 10",
-    "Parallel": "Scenarios",
-    "Executed": "Remote"
-  }
-};
-
-reporter.generate(options);
